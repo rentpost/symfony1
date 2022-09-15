fb2118a094280a81f687db70afd887d1e9ac53c6
Jacob Thomason <jacob@thomason.xxx>
2022-09-15 13:59:24 -0400

    New attempt to try and fix Symfony's cache dir issue :/

--- lib/config/sfConfigCache.class.php
+++ lib/config/sfConfigCache.class.php
@@ -338,19 +338,16 @@ class sfConfigCache
 
     // Seems like there may be a situation where we're getting a file written instead of a dir at
     // /tmp/cache/mypost/dev/config.  Checking if it's a file, removing it and then continuing on.
-    if (file_exists($cacheDir) && !is_dir($cacheDir))
+    if (!is_dir($cacheDir))
     {
-        unlink($cacheDir);
-    }
+        if (file_exists($cacheDir)) {
+          unlink($cacheDir);
+        }
 
-
-    try {
-      if (!is_dir($cacheDir) && !@mkdir($cacheDir, 0777, true) && !is_dir($cacheDir))
-      {
+        if (!mkdir($cacheDir, 0777, true) && !is_dir($cacheDir))
+        {
           throw new \sfCacheException(sprintf('Failed to make cache directory "%s" while generating cache for configuration file "%s".', $cacheDir, $config));
-      }
-    } catch (\Throwable $e) {
-        throw new \sfCacheException(sprintf('Failed to make cache directory "%s" while generating cache for configuration file "%s".', $cacheDir, $config), 0, $e);
+        }
     }
 
     $tmpFile = tempnam($cacheDir, basename($cache));
